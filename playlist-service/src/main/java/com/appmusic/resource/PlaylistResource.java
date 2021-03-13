dd593549182359dd7d6f62b5c89e622cc524827b
Joao Marcos R de Carvalho <joaomarcosribeiroc@gmail.com>
2021-03-13 01:15:13 -0300

    fixing types

--- playlist-service/src/main/java/com/appmusic/resource/PlaylistResource.java
+++ playlist-service/src/main/java/com/appmusic/resource/PlaylistResource.java
@@ -1,8 +1,9 @@
 package com.appmusic.resource;
 
+import javax.ws.rs.QueryParam;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.appmusic.model.PlaylistPojo;
@@ -13,17 +14,16 @@ public class PlaylistResource {
 	
 	@GetMapping
 	public PlaylistPojo getPlaylistSuggestion(
-			@RequestParam("latitude") double latitude,	
-			@RequestParam("longitude") double longitude,
-			@RequestParam("cityname") String cityname
+			@QueryParam("latitude") Double latitude,	
+			@QueryParam("longitude") Double longitude,
+			@QueryParam("cityname") String cityname
  			) {
-		
-		
-		
+
 		PlaylistPojo playlistPojo = new PlaylistPojo();
 		
-		playlistPojo.getSongList().add("Music01");
-		playlistPojo.getSongList().add("Music01");
+		playlistPojo.getSongList().add(String.valueOf(latitude));
+		playlistPojo.getSongList().add(String.valueOf(longitude));
+		playlistPojo.getSongList().add(String.valueOf(cityname));
 		
 		return playlistPojo;
 	}
